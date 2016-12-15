290e5c3c34cc91bead0b06114108bf6de293c089
xianduan <quqinglei@icloud.com>
2016-12-15 15:29:04 +0800

    setting updates

--- project/lqconfig/settings.py
+++ project/lqconfig/settings.py
@@ -146,9 +146,11 @@ MEDIA_ROOT = 'static/uploads/'
 # Rest framework paging setting added by @xianduan
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTH_ENTICATION_CLASSES': (
     #'rest_framework.authentication.BasicAuthentication',
     'rest_framework.authentication.SessionAuthentication',
+    ),
 }
 
 AUTHENTICATION_BACKENDS = (
