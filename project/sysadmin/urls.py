f0667cbce5d5fcd921c43cf77b96a856c7a8d301
xianduan <quqinglei@icloud.com>
2016-12-15 15:21:04 +0800

    update

--- project/sysadmin/urls.py
+++ project/sysadmin/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import include
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
-from views.api_sysadmin import OperationView
 
 urlpatterns = [
 ]
