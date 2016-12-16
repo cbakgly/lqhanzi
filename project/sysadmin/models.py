bb3b157e69b847e3f38cebef35ee813b1d6df071
xianduan <quqinglei@icloud.com>
2016-12-16 18:09:38 +0800

    add user management api code

--- project/sysadmin/models.py
+++ project/sysadmin/models.py
@@ -1,3 +1,5 @@
+# -*- coding:utf8 -*-
+# encoding: utf-8
 from __future__ import unicode_literals
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
@@ -5,18 +7,19 @@ from django.db import models
 from guardian.mixins import GuardianUserMixin
 
 class User(AbstractUser, GuardianUserMixin):
-    gender = models.CharField(max_length=1, blank=True)
-    mb = models.CharField(max_length=32, blank=True)
-    qq = models.CharField(max_length=32, blank=True)
-    address = models.CharField(max_length=64, blank=True)
-    avatar = models.FileField()
+    gender_choice = ((0, 'M'), (1, 'F'))
+    gender = models.IntegerField(u'性别', choices=gender_choice, default=0)
+    mb = models.CharField(u'手机号', max_length=32, blank=True)
+    qq = models.CharField(u'腾讯QQ', max_length=32, blank=True)
+    address = models.CharField(u'地址', max_length=64, blank=True)
+    avatar = models.FileField(u'头像')
 
 # Operation log table
 class Operation(models.Model):
     user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
-    logtype = models.IntegerField()
-    message = models.CharField(max_length=1024)
-    logtime = models.DateTimeField(auto_now_add=True)
+    logtype = models.IntegerField(u'日志类型')
+    message = models.CharField(u'日志内容', max_length=1024)
+    logtime = models.DateTimeField(u'记录时间', auto_now=True)
 
     def __unicode__(self):
         return self.message
--- project/sysadmin/views/api_sysadmin.py
+++ project/sysadmin/views/api_sysadmin.py
@@ -1,8 +1,38 @@
+# encoding: utf-8
+from __future__ import unicode_literals
 from sysadmin.models import User
 from sysadmin.models import Operation
 from rest_framework import viewsets
 from rest_framework import serializers
 import rest_framework_filters as filters
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'gender', 'mb', 'qq', 'address')
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for key in ('username', 'first_name', 'last_name', 'email', 'is_active', 'gender', 'mb', 'qq'):
+            setattr(instance, key, validated_data.get(key, getattr(instance, key)))
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+        
+class UserFilter(filters.FilterSet):
+    class Meta:
+        model = User
+        fields = {'username', 'first_name', 'last_name', 'email', 'is_active', 'gender', 'mb'}
+        
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    filter_class = UserFilter
+    queryset = User.objects.all()
 
 class OperationSerializer(serializers.ModelSerializer):
     class Meta:
