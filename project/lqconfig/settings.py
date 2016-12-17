39a322adc564f5f7246e914b58ab1ee75ddfe1f8
Ye Liu <cbakgly@gmail.com>
2016-12-17 16:29:26 +0800

    add static dir in settings

--- project/lqconfig/settings.py
+++ project/lqconfig/settings.py
@@ -143,6 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'static/'
+]
 
 # User uploaded files
 MEDIA_ROOT = 'static/uploads/'
