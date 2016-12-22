a6313af72eac6ddcbd94bb541a1ddde8de95008f
xianduan <quqinglei@icloud.com>
2016-12-22 16:33:53 +0800

    split functions to different files

--- project/sysadmin/api_urls.py
+++ project/sysadmin/api_urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
-from sysadmin.views.api_sysadmin import OperationViewSet
-from sysadmin.views.api_sysadmin import UserViewSet
+from sysadmin.views.api_operation import OperationViewSet
+from sysadmin.views.api_user import UserViewSet
 
 router = DefaultRouter()
 router.register(r'^operation', OperationViewSet)
---
+++ project/sysadmin/views/api_operation.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+from sysadmin.models import User
+from sysadmin.models import Operation
+from rest_framework import viewsets
+from rest_framework import serializers
+from rest_framework.decorators import detail_route, list_route
+import rest_framework_filters as filters
+from rest_framework.response import Response
+
+# Operation log management
+class OperationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Operation
+        fields = ('id', 'message', 'logtype', 'logtime', 'user')
+
+    def create(self, validated_data):
+        user = validated_data['user']
+        operation = Operation(user=user, logtype=validated_data['logtype'], message=validated_data['message'])
+        operation.save()
+        return Operation(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.user = validated_data.get('user', instance.user)
+        instance.logtype = validated_data.get('logtype', instance.logtype)
+        instance.message = validated_data.get('message', instance.message)
+        instance.save()
+        return instance
+
+
+class OperationFilter(filters.FilterSet):
+    logtime = filters.DateFromToRangeFilter()
+    class Meta:
+        model = Operation
+        fields = {
+                'logtype': ['exact'],
+                'user__username': ['exact'],
+                'message': ['in', 'startswith'],
+        }
+
+
+class OperationViewSet(viewsets.ModelViewSet):
+    serializer_class = OperationSerializer
+    filter_class = OperationFilter
+    queryset = Operation.objects.all()
