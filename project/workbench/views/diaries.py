8591f87fab0959e38abd574289d61a0e04889176
Ye Liu <cbakgly@gmail.com>
2016-12-23 10:42:39 +0800

    add authentication for diaries.

--- project/workbench/views/diaries.py
+++ project/workbench/views/diaries.py
@@ -1,7 +1,10 @@
 # -*- coding:utf8 -*-
 from django.shortcuts import render
 from rest_framework import viewsets
-import django_filters
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.decorators import login_required
+import django_filters.rest_framework
 
 from ..pagination import SmallResultsSetPagination
 from ..serializer import DiarySerializer, TagSerializer, CreditSerializer
@@ -13,6 +16,9 @@ class DiaryViewSet(viewsets.ModelViewSet):
     """
     允许查看打卡记录的API endpoint
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Diaries.objects.all()
     serializer_class = DiarySerializer
     filter_class = wb_filter.DiaryFilter
@@ -23,6 +29,9 @@ class TagViewSet(viewsets.ModelViewSet):
     """
     允许查看打卡类型的API endpoint
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
@@ -31,6 +40,9 @@ class CreditViewSet(viewsets.ModelViewSet):
     """
     积分
     """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Credits.objects.all()
     serializer_class = CreditSerializer
     filter_class = wb_filter.CreditFilter
@@ -38,5 +50,6 @@ class CreditViewSet(viewsets.ModelViewSet):
     pagination_class = SmallResultsSetPagination
 
 
+@login_required
 def index(request):
     return render(request, 'diaries.html')
