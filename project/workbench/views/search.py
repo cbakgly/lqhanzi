d8de4176f36e19426c83c1c5838f95a0d5b56103
Ye Liu <cbakgly@gmail.com>
2016-12-26 16:29:36 +0800

    update routes and view functions.

--- project/workbench/views/search.py
+++ project/workbench/views/search.py
@@ -2,5 +2,25 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'hanzi_search.html')
+def lq_hanzi_db_search(request):
+    return render(request, 'lq_hanzi_db_search.html')
+
+
+def lq_split_db_search(request):
+    return render(request, 'lq_split_db_search.html')
+
+
+def cn_dict_input_db_search(request):
+    return render(request, 'cn_dict_input_db_search.html')
+
+
+def cn_dict_dedup_db_search(request):
+    return render(request, 'cn_dict_dedup_db_search.html')
+
+
+def korean_dedup_db_search(request):
+    return render(request, 'korean_dedup_db_search.html')
+
+
+def korean_taiwan_dedup_db_search(request):
+    return render(request, 'korean_taiwan_dedup_db_search.html')
