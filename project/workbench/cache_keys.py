8101a43c9cd55363b25cdf0718f387b1ca94aff5
Ye Liu <cbakgly@gmail.com>
2016-12-29 19:57:08 +0800

    fix unclosed tag and minor refactoring

--- project/workbench/cache_keys.py
+++ project/workbench/cache_keys.py
@@ -5,4 +5,9 @@
 # 以getcachekey开头作为函数名称
 
 def getcachekey_today_completed_task_num(user_id, business_type):
-    return 'user:' + str(user_id) + (':num_of_today_completed_task:busi_type:') + str(business_type)
+    return 'user:' + str(user_id) + ':num_of_today_completed_task:busi_type:' + str(business_type)
+
+
+def getcachekey_today_credits(user_id):
+    return 'user:' + str(user_id) + ':today_credits'
+
--- project/workbench/views/diaries.py
+++ project/workbench/views/diaries.py
@@ -1,12 +1,10 @@
 # -*- coding:utf8 -*-
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.template import Context
-from .credits import get_user_today_and_total_credits
+from .common import get_today_credits
 
 
 @login_required
 def index(request):
-    c = Context()
-    c.update(get_user_today_and_total_credits(request.user.id))
-    return render(request, 'diaries.html', c)
+    today_credits = get_today_credits(request.user.id)
+    return render(request, 'diaries.html', {"today_credits": today_credits})
