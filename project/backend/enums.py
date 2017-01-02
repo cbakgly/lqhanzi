f2feb86337e9c37080cdfc3b39f2dbfa1b095f36
Ye Liu <cbakgly@gmail.com>
2017-01-02 21:29:48 +0800

    filter migrations and move files

--- project/backend/enums.py
+++ project/backend/enums.py
@@ -6,3 +6,8 @@
 def getenum_business_status(name):
     status = {"ongoing": 0, "completed": 1}
     return status[name]
+
+
+def getenum_business_type(name):
+    type = {"input": 0, "split": 1, "dedup": 2, "help": 3}
+    return type[name]
