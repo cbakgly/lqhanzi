31c0daed85ea23155b49a80e16a384fe6a32a9fa
wangwei <3511622339@qq.com>
2017-02-16 14:43:34 +0800

    完成积分排名

--- project/static/js/workbench/credits-ranking.js
+++ project/static/js/workbench/credits-ranking.js
@@ -10,18 +10,13 @@ var indexOf = function (value) {
         }
     }
 }
-//var username = "ww";
 var appvm = new Vue({
     delimiters: ["#[", "]"],
     el: '#app',
     data: {
-        username: "",
+        searchname: "",
         selected: 1,
         cdata: [],
-        //pagination: {},
-        //page_size: 10,
-        //current_page: 1,
-        //first_entry: true,
         offset: 0,
         mycredit: 0,
         myrank: 1,
@@ -31,49 +26,20 @@ var appvm = new Vue({
 
     },
     methods: {
-        trim: function(stri) {
-            return stri.replace(/(^\s*)|(\s*$)/g, ""); },
-        gen_url: function () {
-
-            var username = document.getElementsByClassName("hidden-xs")[0].innerHTML;
-            username = appvm.trim(username);
-
-            base_url += "&user__username=" + username
-            //base_url +=
-            return base_url
-        },
-        /*get_offset: function () {
-            var url = this.gen_url()
-            var app = this
-            axios({
-                method:"get",
-                url: url,
-                xsrfCookieName: 'csrftoken',
-                xsrfHeaderName: 'X-CSRFToken'
-            })
-                .then(function(data){
-                    for (var i = 0; i < data.length; i++) {
-                        if (indexof(data[i].sort) == app.selected) {
-                            app.offset = data[i].rank - 2
-                            break
-                        }
-                    }
-                }).catch(function(error){
-                    console.log(error);
-            })
-        },*/
         load_pages: function () {
-            //this.current_page = page_num
-            //appvm.get_offset()
-            var url = "/api/v1/credits/?sort=" + appvm.selected;
-            //url = url + "&offset=" + appvm.offset
+            var url = "/api/v1/credits/searchcredit/";
             $.ajax({
                 url: url,
+                method:'GET',
+                enctype : "multipart/form-data",
                 dataType: 'json',
+                data:{
+                    'select_sort':this.selected,
+                    'search_name':this.searchname
+                },
                 cache: false,
                 success: function (data) {
                     appvm.cdata = data;
-                    //appvm.pagination = data.pagination
                 },
                 error: function (xhr, status, err) {
                     console.log('error');
@@ -82,14 +48,12 @@ var appvm = new Vue({
         },
         get_login_user: function () {
             var base_url = "/api/v1/credits/certain_user_credits/";
-
-            //console.log(username)
-            //var url = base_url+"&user__username="+username+"&sort=1"
             $.ajax({
                 url: base_url,
                 dataType: 'json',
                 cache: false,
                 success: function (data) {
+                    console.log(data);
                     appvm.cdata = data;
                     for (var i = 0; i < data.length; i++) {
                         if (data[i].sort == 0) {
