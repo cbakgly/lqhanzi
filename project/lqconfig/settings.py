aacfa0b6fdd988cc3f75f141aad38d2119bae9a3
Ye Liu <cbakgly@gmail.com>
2017-03-02 16:27:33 +0800

    update settings and urls

--- project/lqconfig/settings.py
+++ project/lqconfig/settings.py
@@ -13,21 +13,73 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+SECRET_KEY = 'nej4uf&$0-18*bb)--qjx3vc6-ahsxt!c!z92g^h$qjl4036*x'
+
+DEBUG = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nej4uf&$0-18*bb)--qjx3vc6-ahsxt!c!z92g^h$qjl4036*x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 ALLOWED_HOSTS = ["*", ]
 
-# Use S3 path to get hanzi pictures
+ROOT_URLCONF = 'lqconfig.urls'
+WSGI_APPLICATION = 'lqconfig.wsgi.application'
+AUTH_USER_MODEL = 'backend.User'
+
+# For customized user model, guardian must be tuned
+# GUARDIAN_MONKEY_PATCH = False
+# INTERNAL_IPS = ('127.0.0.1',)
+ADMINS = [('Xiandu', 'lqs.xiandu@qq.com')]
+SERVER_EMAIL = 'root@hanzi.lqdzj.cn'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+LANGUAGE_CODE = 'zh-Hans'
+TIME_ZONE = 'Asia/Shanghai'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
 USE_S3_HANZI_PICTURE = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/uploads/')
+
+# Registration Settings
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/workbench/'
+REGISTRATION_EMAIL_SUBJECT_PREFIX = '[Please verify your email account for lqhanzi.]'
+SEND_ACTIVATION_EMAIL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'alqdzj@126.com'  # 账号密码同aws密码
+EMAIL_HOST_PASSWORD = 'dongpeilou404'
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+EMAIL_HOST = 'smtp.126.com'
+EMAIL_PORT = 25
+# Registration Settings end
+
+# SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_COOKIE_HTTPONLY = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SECURE_REDIRECT_EXEMPT = ['index', 'stroke_', 'inverse_', 'variant_', 'dicts\/', '']
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+CACHE_MIDDLEWARE_SECONDS = 10
+APPEND_SLASH = True
 
 # Application definition
 INSTALLED_APPS = [
@@ -51,17 +103,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
-ROOT_URLCONF = 'lqconfig.urls'
 
 TEMPLATES = [
     {
@@ -83,22 +136,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lqconfig.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lqhanzi',
-        'USER': 'lq',
-        'PASSWORD': '123456',
-        'HOST': '192.168.16.3',
-        # 'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4', 'init_command': 'SET default_storage_engine=InnoDB'},
-    }
-}
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
@@ -125,29 +162,6 @@ AUTHENTICATION_BACKENDS = (
     # 'guardian.backends.ObjectPermissionBackend'
 )
 
-# For abstract user
-AUTH_USER_MODEL = 'backend.User'
-
-# For customized user model, guardian must be tuned
-# GUARDIAN_MONKEY_PATCH = False
-# INTERNAL_IPS = ('127.0.0.1',)
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-LANGUAGE_CODE = 'zh-Hans'
-TIME_ZONE = 'Asia/Shanghai'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/uploads/')
-
 # Rest framework paging setting added by @xianduan
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
@@ -159,42 +173,28 @@ REST_FRAMEWORK = {
     ),
 }
 
-# Registration Settings
-# If True, users can register
-REGISTRATION_OPEN = True
-# One-week activation window; you may, of course, use a different value.
-ACCOUNT_ACTIVATION_DAYS = 3
-# If True, the user will be automatically logged in.
-REGISTRATION_AUTO_LOGIN = True
-# The page you want users to arrive at after they successfully log in
-LOGIN_REDIRECT_URL = '/workbench/'
-# The page users are directed to if they are not logged in,
-# and are trying to access pages requiring authentication
-LOGIN_URL = '/accounts/login/'
-REGISTRATION_EMAIL_SUBJECT_PREFIX = '[龙泉字库注册邮件]'
-SEND_ACTIVATION_EMAIL = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'alqdzj@126.com'  # 账号密码同aws密码
-EMAIL_HOST_PASSWORD = 'dongpeilou404'
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
-EMAIL_HOST = 'smtp.126.com'
-EMAIL_PORT = 25
-# Registration Settings end
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'lqhanzi',
         'OPTIONS': {
-            'MAX_ENTRIES': 1000
+            'MAX_ENTRIES': 500
         },
     },
 }
 
-# SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-# SESSION_COOKIE_HTTPONLY = True
-# SESSION_COOKIE_SECURE = True
-
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SECURE_REDIRECT_EXEMPT = ['index', 'stroke_', 'inverse_', 'variant_', 'dicts\/', '']
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lqhanzi',
+        'USER': 'lq',
+        'PASSWORD': '123456',
+        'HOST': '192.168.16.3',
+        # 'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4', 'init_command': 'SET default_storage_engine=InnoDB'},
+    }
+}
