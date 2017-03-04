b820be9f430f18e2a56af2108d2b7e54b48d70e5
Ye Liu <cbakgly@gmail.com>
2017-03-05 05:27:13 +0800

    add lqhanzi font in m2

--- project/backend/context_processors.py
+++ project/backend/context_processors.py
@@ -1,6 +1,6 @@
 # -*- coding:utf8 -*-
 import json
-from backend.utils import get_today_credits
+from backend.utils import get_today_credits, get_lqhanzi_font_path
 from backend.enums import TASK_PACKAGE_STATUS_ENUM, TASK_STATUS_ENUM, BUSINESS_TYPE_ENUM, BUSINESS_STAGE_ENUM
 from backend.models import BUSINESS_STAGE_CHOICES, BUSINESS_TYPE_CHOICES, INPUT_VARIANT_TYPE_CHOICES
 
@@ -21,4 +21,10 @@ def model_enum(request):
         "input_variant_type_choices": INPUT_VARIANT_TYPE_CHOICES,
         "business_type_choices_json": json.dumps(dict(BUSINESS_TYPE_CHOICES), encoding="UTF-8", ensure_ascii=False),
         "business_stage_choices_json": json.dumps(dict(BUSINESS_STAGE_CHOICES), encoding="UTF-8", ensure_ascii=False),
+    }
+
+
+def lqhanzi_font(request):
+    return {
+        'lqhanzi_font_path': get_lqhanzi_font_path(),
     }
